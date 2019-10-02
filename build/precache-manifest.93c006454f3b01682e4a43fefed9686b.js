5ebede75971c83d486ce174f05fbc380397f7e25
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2019-10-02 10:22:21 -0400

    Revert "Url updates"
    This reverts commit 3d997db2d7d8370ec96a98af7e7a467e9653aa51.

--- build/precache-manifest.93c006454f3b01682e4a43fefed9686b.js
+++ build/precache-manifest.93c006454f3b01682e4a43fefed9686b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c72719f6badeac161f4efd344ef0cc2",
+    "revision": "c7b6d6d5d30161e106dc3998457f7069",
     "url": "/wellik-site/index.html"
   },
   {
-    "revision": "c67e75c37f4f93da2672",
-    "url": "/wellik-site/static/css/main.a20f92f5.chunk.css"
+    "revision": "b70a37af5cd000ade202",
+    "url": "/wellik-site/static/css/main.6dd8bb8d.chunk.css"
   },
   {
     "revision": "1fa49146d808113ba054",
     "url": "/wellik-site/static/js/2.393ae48b.chunk.js"
   },
   {
-    "revision": "c67e75c37f4f93da2672",
-    "url": "/wellik-site/static/js/main.984287b9.chunk.js"
+    "revision": "b70a37af5cd000ade202",
+    "url": "/wellik-site/static/js/main.3e7a0683.chunk.js"
   },
   {
     "revision": "c81ad85130002dfc3305",
