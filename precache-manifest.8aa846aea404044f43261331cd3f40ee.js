5467c8572cef570f575b5edda0441a40b54fa299
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2019-10-02 09:56:56 -0400

    Updates

---
+++ precache-manifest.8aa846aea404044f43261331cd3f40ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac050b7e827a6cb400ca0fedd896c108",
+    "url": "/wellik-site/index.html"
+  },
+  {
+    "revision": "4b1491a07a996d05dc7e",
+    "url": "/wellik-site/static/css/main.6dd8bb8d.chunk.css"
+  },
+  {
+    "revision": "1fa49146d808113ba054",
+    "url": "/wellik-site/static/js/2.393ae48b.chunk.js"
+  },
+  {
+    "revision": "4b1491a07a996d05dc7e",
+    "url": "/wellik-site/static/js/main.692783e7.chunk.js"
+  },
+  {
+    "revision": "c81ad85130002dfc3305",
+    "url": "/wellik-site/static/js/runtime-main.fa419024.js"
+  }
+]);
