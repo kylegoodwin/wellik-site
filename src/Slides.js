1be86c7629b789f3922fde0a231f1b1dda37b5bc
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2019-10-09 22:11:01 -0400

    kind of working but not really

--- src/Slides.js
+++ src/Slides.js
@@ -1,39 +1,84 @@
 import React from 'react';
 
 
+class LoadVideo extends React.Component{
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      loaded: false
+    }
+  }
+
+  videoLoaded = () => {
+    this.setState({
+      loaded: true
+    })
+  }
+
+  render(){
+    return(
+    <div className="video-wrap">
+      {/*<span className="left-text">pare down to the essence</span>*/}
+      <video  onLoadedData={this.videoLoaded} id={"img-loaded" } autoPlay muted loop src={this.props.url}></video>
+      {/*<span className="bottom-text">but don’t remove the poetry</span>*/}
+    </div>)
+  }
+
+
+}
+
+
 const one =  (
+  <div>
     <div className="main-image">
-    <div className="main-logo">
+    <LoadVideo url="/home.mp4"></LoadVideo>
       <h1>bethany wellik</h1>
       <h2>productivity + organization</h2>
-    </div>
-    <div className="bottom-bar">
-          <div>
-            <p>
-              789 Sik Block <br></br>
-              New York, NY 10003
-            </p>
-          </div>
         </div>
-  </div>)
+        <div className="bottom-bar">
+      <div>
+        <p>
+          789 Sik Block <br></br>
+          New York, NY 10003
+        </p>
+      </div>
+        </div>
+          </div>
+  )
 
 const two = (
-    <video autoPlay muted loop className="slide-two" src="/new.mp4">
-    </video>
+    <div className="slide-two">
+      <LoadVideo url="/new.mp4" />
+    </div>
+    
 )
 
 const three = (
     <div className="slide-three">
-    <video autoPlay muted loop src="/two.mp4">
-    </video>
+    <span className="left-text">pare down to the essence</span>
+    <LoadVideo url="/two2.mp4" />
+    <span className="bottom-text">pare down to the essence</span>
     </div>
 
+)
+
+const four = (
+  <div className="slide-four">
+    <LoadVideo url="/three.mp4"></LoadVideo>
+  </div>
+)
+
+const five = (
+  <LoadVideo url="/four.mp4"></LoadVideo>
 )
 
 const slides = [
     one,
     three,
-    two
+    two,
+    four,
+    five
 ]
 
 
